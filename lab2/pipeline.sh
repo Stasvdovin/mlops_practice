5e0c291a560d3d4207662dfca970c9dae5292190
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-05-25 04:50:51 +0500

    Update pipeline.sh

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install scikit-learn
-python3 data_creation.py
-python3 data_preprocessing.py
-python3 model_preparation.py
-python3 model_testing.py | awk '{print "Mean squared error: %.2f", $4}'
+python3 /lab2/data_creation.py
+python3 /lab2/data_preprocessing.py
+python3 /lab2/model_preparation.py
+python3 /lab2/model_testing.py | awk '{print "Mean squared error: %.2f", $4}'
