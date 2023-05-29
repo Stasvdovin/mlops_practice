8b0c88e8a1653fbe2ecb11dc7f168485f272662a
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-05-30 03:07:18 +0500

    Update pipeline.sh

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -4,7 +4,7 @@ pip install pandas
 pip install numpy
 find /lab2 -type d -exec chmod 777 {} \;
 find /lab2 -type f -exec chmod 777 {} \;
-python3 /lab2/data_creation.py
-python3 /lab2/data_preprocessing.py
-python3 /lab2/model_preparation.py
-python3 /lab2/model_testing.py | awk '{print "Mean squared error: %.2f", $4}'
+python3 /var/lib/jenkins/workspace/test/lab2/data_creation.py
+python3 /var/lib/jenkins/workspace/test/lab2/data_preprocessing.py
+python3 /var/lib/jenkins/workspace/test/lab2/model_preparation.py
+python3 /var/lib/jenkins/workspace/test/lab2/model_testing.py | awk '{print "Mean squared error: %.2f", $4}'
