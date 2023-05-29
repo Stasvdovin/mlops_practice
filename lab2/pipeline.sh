7fd9e8bb7b6b4461ef2202b0224e026d62c26aaa
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-05-30 03:06:19 +0500

    Update pipeline.sh

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 pip install scikit-learn
-python3 data_creation.py
+pip install pandas
+pip install numpy
+find /lab2 -type d -exec chmod 777 {} \;
+find /lab2 -type f -exec chmod 777 {} \;
+python3 /lab2/data_creation.py
 python3 /lab2/data_preprocessing.py
 python3 /lab2/model_preparation.py
 python3 /lab2/model_testing.py | awk '{print "Mean squared error: %.2f", $4}'
