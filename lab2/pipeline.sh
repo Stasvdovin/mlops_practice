ae1f7eceda81bef5f15666b36576ad5ec44d1f50
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-05-30 03:29:06 +0500

    Update pipeline.sh

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
+python3 -m pip install -r var/lib/jenkins/workspace/test/lab2/requirements.txt 
 
-pip3 install -r var/lib/jenkins/workspace/test/lab2/requirements.txt
 python3 /var/lib/jenkins/workspace/test/lab2/data_creation.py
 python3 /var/lib/jenkins/workspace/test/lab2/data_preprocessing.py
 python3 /var/lib/jenkins/workspace/test/lab2/model_preparation.py
