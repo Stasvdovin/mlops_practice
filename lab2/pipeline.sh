308cae4d4e05517de20401ab40d451c386485b12
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-05-30 03:23:33 +0500

    Update pipeline.sh

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip install pandas
-pip install numpy
-pip install scikit-learn
 
+pip install -r var/lib/jenkins/workspace/test/lab2/requirements.txt
 python3 /var/lib/jenkins/workspace/test/lab2/data_creation.py
 python3 /var/lib/jenkins/workspace/test/lab2/data_preprocessing.py
 python3 /var/lib/jenkins/workspace/test/lab2/model_preparation.py
